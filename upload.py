5f47814d155604ecb971b9fcb66059c5309d14c1
Abhishek Dasgupta <55708520+Abhishek010397@users.noreply.github.com>
2020-12-10 19:32:00 +0530

    Code Fixed Issue Resolved

--- upload.py
+++ upload.py
@@ -3,12 +3,8 @@ from botocore.exceptions import NoCredentialsError
 
 
 def upload_to_aws(file_s3, BUCKET_NAME, s3_file):
-    sts = boto3.client('sts')
-    sts_result = sts.assume_role(
-        RoleArn='arn:aws:iam::680763698946:role/EC2S3Access', RoleSessionName='session')
-    s3 = boto3.client('s3', aws_access_key_id=sts_result['Credentials']['AccessKeyId'],
-                      aws_secret_access_key=sts_result['Credentials']['SecretAccessKey'],
-                      aws_session_token=sts_result['Credentials']['SessionToken'])
+
+    s3 = boto3.client('s3')
 
     try:
         s3.upload_file(file_s3, BUCKET_NAME, s3_file)
@@ -22,4 +18,4 @@ def upload_to_aws(file_s3, BUCKET_NAME, s3_file):
         return False
 
 
-uploaded = upload_to_aws('./vpc.txt', 'lambdabucket-cerebrone', 's3_file')
+uploaded = upload_to_aws('./vpc.tf', 'lambdabucket-cerebrone', 's3_file')
