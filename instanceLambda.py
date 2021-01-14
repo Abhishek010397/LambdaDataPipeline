83e191911e827a2b0d87d98ff25ad96a17d300fd
Abhishek Dasgupta <55708520+Abhishek010397@users.noreply.github.com>
2021-01-14 22:35:49 +0530

    Updates

--- instanceLambda.py
+++ instanceLambda.py
@@ -1,7 +1,7 @@
 import boto3
 region = 'us-west-1'
-instances = ['<your-instances-id>', 'can be multiple id']
-ec2 = boto3.client('ec2', region_name=your_region_name)
+instances = ['<your-instances-id>', <'can be multiple id'...>]
+ec2 = boto3.client('ec2', region_name=region)
 
 
 def lambda_handler(event, context):
